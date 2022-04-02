29e01aa9ab848b7d0222339f3e264c338ed2731f
Eugene Murray <eugene.murray@outlook.com>
2022-04-02 16:20:05 +0100

    Add some algos

---
+++ Linearsearch/Program_LinearSearch.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Algos Linear Search");
+
+static Boolean LinearSearch(int[] input, int n) {
+    foreach(int current in input) {
+        if (n == current) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int[] arr = {1, 2, 3, 4, 5, 6};
+
+// Via Custom Method
+Console.WriteLine(LinearSearch(arr, 5));
+
+// Via build-in methods
+int item = Array.Find(arr, element => element == 3);
+Console.WriteLine(item);
+
+int[] items = Array.FindAll(arr, element => element >= 5);
+Array.ForEach(items, Console.WriteLine);
+
+
+
+
+
+
+
